208e0ec39d67a566e58d920e4ef11bb0c8e7696f
Naman Patel <46571853+np453@users.noreply.github.com>
2021-05-15 21:40:11 +0530

    email verify route added

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
+const AWS = require('aws-sdk');
 const User = require('../models/user');
 const Events = require('../models/events');
+
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.aws_access_key;
+AWS.config.secretAccessKey = process.env.aws_secret_key;
+AWS.config.region = process.env.aws_reigon;
+
+
+var ses = new AWS.SES();
+
 router.post('/update', async(req, res)=>{
     try{
         const event = await Events.findByIdAndUpdate(req.body.eventid,{
@@ -12,10 +22,37 @@ router.post('/update', async(req, res)=>{
                     type:req.body.type
                 }
             })
+            const email=req.body.useremail;
+            var ses_mail = "From: '1999Sharp' <" + email + ">\n";
+            ses_mail = ses_mail + "To: " + email + "\n";
+            ses_mail = ses_mail + "Subject: Event Updated Sucessfully\n";
+            ses_mail = ses_mail + "MIME-Version: 1.0\n";
+            ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+            ses_mail = ses_mail + "--NextPart\n";
+            ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+            ses_mail = ses_mail + "This is should be a html text.\n\n";
+            ses_mail = ses_mail + "--NextPart\n";
+            ses_mail = ses_mail + "Content-Type: text/plain;\n";
+            ses_mail = ses_mail + "Your Event "+ req.body.title + "has been updated!!";
+            ses_mail = ses_mail + "--NextPart--";
+            var params = {
+                RawMessage: { Data: new Buffer.from(ses_mail) },
+                Destinations: [ email ],
+                Source: "'1999Sharp' <" + email + ">'"
+            };
+            ses.sendRawEmail(params, function(err, data) {
+                if(err) {
+                    res.send(err);
+                } 
+                else {
+                    res.send(data);
+                }           
+            });
         res.send('success');
     }catch(e){
         res.send(e);
     }
+
 })
 
 router.get('/user/:eventid', async(req, res) => {
@@ -43,9 +80,46 @@ router.post('/new', async(req, res)=>{
                 event: savedevent._id
             }
         })
+        const email=req.body.useremail;
+            var ses_mail = "From: '1999Sharp' <" + email + ">\n";
+            ses_mail = ses_mail + "To: " + email + "\n";
+            ses_mail = ses_mail + "Subject: Event Updated Sucessfully\n";
+            ses_mail = ses_mail + "MIME-Version: 1.0\n";
+            ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+            ses_mail = ses_mail + "--NextPart\n";
+            ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+            ses_mail = ses_mail + "This is should be a html text.\n\n";
+            ses_mail = ses_mail + "--NextPart\n";
+            ses_mail = ses_mail + "Content-Type: text/plain;\n";
+            ses_mail = ses_mail + "Your Event "+ req.body.title + "has been Added!!";
+            ses_mail = ses_mail + "--NextPart--";
+            var params = {
+                RawMessage: { Data: new Buffer.from(ses_mail) },
+                Destinations: [ email ],
+                Source: "'1999Sharp' <" + email + ">'"
+            };
+            ses.sendRawEmail(params, function(err, data) {
+                if(err) {
+                    res.send(err);
+                } 
+                else {
+                    res.send(data);
+                }           
+            });
         res.send("success");
     }catch(e){
         res.send(e);
     }
 })
+router.post('/delete/:eventid', async (req, res) => {
+    const event = Events.findByIdAndRemove(req.params.eventid);
+    const user = User.findByIdAndUpdate(req.body.userid,{
+        events:{ 
+            $pull:{
+                _id:req.params.eventid
+            }
+        }
+    })
+})
+
 module.exports= router;
---
+++ routes/verify.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const otpGenerator = require('otp-generator');
+const AWS = require('aws-sdk');
+const User = require('../models/user');
+
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.aws_access_key;
+AWS.config.secretAccessKey = process.env.aws_secret_key;
+AWS.config.region = process.env.aws_reigon;
+
+const email="acw.dnsp@gmail.com";
+var ses = new AWS.SES();
+
+router.post('/verify', async (req, res) => {
+    try{
+        const otp=otpGenerator.generate(6, { upperCase: false, specialChars: false, alphabets:false});
+        const user = await User.findByIdAndUpdate(req.body.userid,{
+            $set:{
+                resetpass:otp
+            }
+        })
+        var ses_mail = "From: '1999Sharp' <" + email + ">\n";
+        ses_mail = ses_mail + "To: " + email + "\n";
+        ses_mail = ses_mail + "Subject: Password Reset Attempted\n";
+        ses_mail = ses_mail + "MIME-Version: 1.0\n";
+        ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+        ses_mail = ses_mail + "--NextPart\n";
+        ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+        ses_mail = ses_mail + "This is should be a html text.\n\n";
+        ses_mail = ses_mail + "--NextPart\n";
+        ses_mail = ses_mail + "Content-Type: text/plain;\n";
+        ses_mail = ses_mail + "Your OTP is " + otp + "and is valid for 10 min";
+        ses_mail = ses_mail + "--NextPart--";
+        var params = {
+            RawMessage: { Data: new Buffer.from(ses_mail) },
+            Destinations: [ email ],
+            Source: "'1999Sharp' <" + email + ">'"
+        };
+        ses.sendRawEmail(params, function(err, data) {
+            if(err) {
+                res.send(err);
+            } 
+            else {
+                res.send(data);
+            }           
+        });
+
+    }catch(err) {
+        res.send(err);
+    }
+})
+router.post('/update', async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.body.userid,{
+        $set:{
+            email:req.body.email
+        }
+    })
+})
+router.post('/otp-verify', async (req, res) => {
+    try{
+       const user = await User.findById(req.body.userid);
+        if(user.resetpass==req.body.resetpass){
+            res.status(200).send("verified");
+        }
+        else{
+            res.send("otp mismatch");
+        } 
+    }catch(e){
+        res.send(e);
+    }
+    
+})
+module.exports = router;
